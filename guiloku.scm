b4ebb0d5247bd2d43d3e23d9fd161cf4d008d54a
Muriithi Frederick Muriuki <fredmanglis@gmail.com>
2016-08-13 07:02:30 +0300

    Add initial function
    Add functions to initialise the game, and mark some cells.
    At this point, the vector is not being copied over, making the
    mark-cell function not functional.

---
+++ guiloku.scm
@@ -0,0 +1,17 @@
+(define (init-rows size)
+  (let ((col (make-vector 15 0)))
+    (make-vector 15 (vector-copy col))))
+
+(define  (init-game size)
+  (list (cons "size" size)
+        (cons "turn" 0)
+        (cons "board" (init-rows size))))
+
+;; TODO: Update so that this does a deep-copy
+;;       The vectors are not copied here, which is a problem
+(define (mark-cell row col player game)
+  (let ((cell (cons col player))
+        (new-game (list-copy game)))
+    (let ((sel-row (vector-ref (cdr (list-ref new-game 2)) row)))
+      (vector-set! sel-row col player))
+    new-game))
