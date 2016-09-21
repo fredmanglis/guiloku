3cba193ebcde044b8b8385966cbf3a4aa6779cd0
Muriithi Frederick Muriuki <fredmanglis@gmail.com>
2016-09-21 16:49:45 +0300

    Define the sly package in the guix.scm file
    Initialise the sly definition in the guix.scm file so that it is
    installed as a dependency when we try to build and run guiloku

--- guix.scm
+++ guix.scm
@@ -18,8 +18,29 @@
 (use-modules (guix gexp)
              (guix packages)
              (guix download)
+             (guix git-download)
              (guix build-system gnu)
              (guix licenses))
+
+(define sly
+  (package
+   (name "sly")
+   (version "0.2.0")
+   (source (origin
+            (method git-fetch)
+            (uri (git-reference
+                  (url "git://dthompson.us/sly.git")
+                  (commit "0fe5a817")))
+            (sha256
+             (base32
+              "15jqzxrqlv0kadil5c2b7clvkqpxxgw21nc03z1x467bbbl928lf"))))
+   (build-system gnu-build-system)
+   (synopsis "2D/3D game engine for GNU Guile")
+   (description "Sly is a 2D/3D game engine written in Guile Scheme.
+Sly differs from most game engines in that it emphasizes functional
+reactive programming and live coding.")
+   (home-page "http://dthompson.us/pages/software/sly.html")
+   (license gpl3+)))
 
 (define guiloku
   (package
@@ -33,3 +54,8 @@
    (description "Guiloku implements a game of gomoku, allowing 2 players to play against each other on a 15 by 15 board. It is implemented in Guile")
    (license gpl3+)
    (home-page "https://github.com/fredmanglis/guiloku")))
+
+(define (return-package p)
+  p)
+
+(return-package guiloku)
